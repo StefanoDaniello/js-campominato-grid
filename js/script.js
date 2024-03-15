fc42d4e657792313b61c29d5eee689e085c70625
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-03-15 18:39:39 +0100

    add button

--- js/script.js
+++ js/script.js
@@ -11,20 +11,79 @@ con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 
 con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 */
 
+const button =document.querySelector('.btn')
 
-const squareWrapper=document.createElement('div')
-squareWrapper.className ='d-flex flex-wrap w-100';
-
-
-let numBox =100;
-let tmpHTML = '';
-
-for (i=0 ; i<numBox; i++){
-    let number = i;
-    tmpHTML+=`<div class="box">${number}</div>`
+function newSquaregrid(content){
+    const square =document.createElement('div')
+    square.innerHTML ='<span>' + content + '</span>'
+    square.classList.add('box')
+    return square
+}
+function evenOrodd(number){
+    if(number % 2 === 0){
+        return true
+    }else{
+        return false
+    }
 }
 
-squareWrapper.innerHTML=tmpHTML;
+button.addEventListener('click', function(){
 
-const stContainer=document.querySelector('.st-container');
-stContainer.append(squareWrapper);
+    const stContainer =document.querySelector('.st-container')
+
+    for (let i=0 ; i<100; i++){
+        const newSquare = newSquaregrid(i + 1)
+        newSquare.addEventListener('click', function(){
+            if(evenOrodd(i + 1)){
+                newSquare.classList.add('bgeven')
+            }else{
+                newSquare.classList.add('bgodd')
+            }
+
+        })
+        stContainer.appendChild(newSquare);
+    }
+})
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myfunction(){
+//     const squareWrapper=document.createElement('div')
+//     squareWrapper.className ='d-flex flex-wrap w-100';
+    
+    
+//     let numBox =100;
+//     let tmpHTML = '';
+    
+//     for (i=0 ; i<numBox; i++){
+//         let number = i;
+//         tmpHTML+=`<div class="box">${number}</div>`
+//         console.log(i)
+//     }
+//     squareWrapper.innerHTML=tmpHTML;
+//     const stContainer=document.querySelector('.st-container');
+//     // stContainer.classList.toggle('box')
+//     stContainer.append(squareWrapper);
+// }
+
+
+    
+
+
+
+
+  
+
+
